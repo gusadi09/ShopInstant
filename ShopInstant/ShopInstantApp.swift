d3bd9769644091821dd504b1227acee764dab73b
AgusAdiPranata <agus@ewideplus.com>
2024-09-03 10:06:36 +0700

    Initial Commit

---
+++ ShopInstant/ShopInstantApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopInstantApp.swift
+//  ShopInstant
+//
+//  Created by Ewide Dev 5 on 03/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ShopInstantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
