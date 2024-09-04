3fb18b57b5ba9807c36929bde109c561969f3c41
AgusAdiPranata <agus@ewideplus.com>
2024-09-04 14:05:01 +0700

    [SHOPS] add new component for offers named offer card

---
+++ ShopInstant/Common/Component/Card/OfferCard.swift
@@ -0,0 +1,73 @@
+//
+//  OfferCard.swift
+//  ShopInstant
+//
+//  Created by Ewide Dev 5 on 04/09/24.
+//
+
+import SwiftUI
+
+struct OfferCard: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            GeometryReader(content: { geometry in
+                WebImageLoaderView(
+                    url: "test",
+                    width: geometry.size.width,
+                    height: 120,
+                    aspectRatio: .fit
+                )
+                .frame(width: geometry.size.width, height: 120)
+                .background(RoundedRectangle(cornerRadius: 12).foregroundColor(.gray))
+            })
+            .frame(height: 120)
+            
+            Text("Title With 2 Lines Here")
+                .font(.system(size: 16, weight: .bold))
+                .lineLimit(2)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                HStack(spacing: 10) {
+                    Text("Rp 15.000")
+                        .strikethrough(true, pattern: .solid, color: .red)
+                        .font(.system(size: 12, weight: .regular))
+                    
+                    Text("10%")
+                        .font(.system(size: 10, weight: .semibold))
+                        .foregroundStyle(.pink)
+                        .padding(4)
+                        .background(
+                            RoundedRectangle(cornerRadius: 5)
+                                .foregroundStyle(.red.opacity(0.2))
+                        )
+                }
+                
+                Text("Rp 10.000")
+                    .font(.system(size: 14, weight: .bold))
+                    .foregroundStyle(.red)
+            }
+        }
+        .padding([.top, .horizontal], 8)
+        .padding(.bottom, 10)
+        .background(
+            Rectangle()
+                .foregroundStyle(.background)
+        )
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .shadow(color: .black.opacity(0.1), radius: 6)
+    }
+}
+
+#Preview {
+    ScrollView {
+        GeometryReader(content: { geometry in
+            LazyVGrid(columns: [GridItem(.flexible(minimum: 60, maximum: geometry.size.width)), GridItem(.flexible(minimum: 60, maximum: geometry.size.width))], spacing: 15, content: {
+                ForEach(1...4, id: \.self) { item in
+                    OfferCard()
+                        .padding(item % 2 == 0 ? .leading : .trailing, 5)
+                }
+            })
+            .padding()
+        })
+    }
+}
